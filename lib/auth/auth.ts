c1d4b033223aae4b64c2fe325028d10b4044bf5b
illiaN0viKov <illia.novikov3hunna@gmail.com>
2026-03-30 16:53:42 +0200

    auth-fix

--- lib/auth/auth.ts
+++ lib/auth/auth.ts
@@ -3,9 +3,14 @@ import { mongodbAdapter } from "better-auth/adapters/mongodb";
 import { MongoClient } from "mongodb";
 import { headers } from "next/headers";
 import { initializeUserBoard } from "../init-user-board";
+import connectDB from "../db";
 
-const client = new MongoClient(process.env.MONGODB_URI!)
-const db =client.db()
+// const client = new MongoClient(process.env.MONGODB_URI!)
+// const db =client.db()
+
+const mongooseInstance = await connectDB();
+const client = mongooseInstance.connection.getClient();
+const db = client.db();
 
 export const auth = betterAuth({
     database: mongodbAdapter(db, {
